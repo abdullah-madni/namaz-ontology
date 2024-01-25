9f9f6a04b0cbb62635f6d367feabaeec0d618830
Muhammad Abdullah Madni <abdullahmadni2017@gmail.com>
2024-01-25 13:40:45 +0500

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+Namaz Ontology is a beautiful namaz related multipage website.
+
+# features
+
+* Pyayer Times
+* Tasbeeh counter
+* Islamic books (Quran, Hadees, Seerah, Namaz)
+* Quran & Hadees of the day
+* and more.
+
+You can view website on: https://namaz-ontology-7.netlify.app/
---
+++ js/app.js
@@ -0,0 +1,64 @@
+$(document).ready(function () {
+   showNamazTime('sargodha');
+
+   $('.tasbeeh-trigger').click(function () {
+      let counter = parseInt($('.tasbeeh-counter').html());
+      counter++;
+      $('.tasbeeh-counter').html(counter);
+   });
+
+   $('.tasbeeh-reset').click(function () {
+      $('.tasbeeh-counter').html('0')
+   });
+
+
+   $('#namaz_time_form').submit(function (e) {
+      e.preventDefault();
+      showNamazTime(this.city.value)
+   });
+
+
+
+   let allCities = ["Islamabad", "Ahmed Nager", "Ahmadpur East", "Ali Khan", "Alipur", "Arifwala", "Attock", "Bhera", "Bhalwal", "Bahawalnagar", "Bahawalpur", "Bhakkar", "Burewala", "Chillianwala", "Chakwal", "Chichawatni", "Chiniot", "Chishtian", "Daska", "Darya Khan", "Dera Ghazi", "Dhaular", "Dina", "Dinga", "Dipalpur", "Faisalabad", "Fateh Jhang", "Ghakhar Mandi", "Gojra", "Gujranwala", "Gujrat", "Gujar Khan", "Hafizabad", "Haroonabad", "Hasilpur", "Haveli", "Lakha", "Jalalpur", "Jattan", "Jampur", "Jaranwala", "Jhang", "Jhelum", "Kalabagh", "Karor Lal", "Kasur", "Kamalia", "Kamoke", "Khanewal", "Khanpur", "Kharian", "Khushab", "Kot Adu", "Jauharabad", "Lahore", "Lalamusa", "Layyah", "Liaquat Pur", "Lodhran", "Malakwal", "Mamoori", "Mailsi", "Mandi Bahauddin", "mian Channu", "Mianwali", "Multan", "Murree", "Muridke", "Mianwali Bangla", "Muzaffargarh", "Narowal", "Okara", "Renala Khurd", "Pakpattan", "Pattoki", "Pir Mahal", "Qaimpur", "Qila Didar", "Rabwah", "Raiwind", "Rajanpur", "Rahim Yar", "Rawalpindi", "Sadiqabad", "Safdarabad", "Sahiwal", "Sangla Hill", "Sarai Alamgir", "Sargodha", "Shakargarh", "Sheikhupura", "Sialkot", "Sohawa", "Soianwala", "Siranwali", "Talagang", "Taxila", "Toba Tek", "Vehari", "Wah Cantonment", "Wazirabad", "Badin", "Bhirkan", "Rajo Khanani", "Chak", "Dadu", "Digri", "Diplo", "Dokri", "Ghotki", "Haala", "Hyderabad", "Islamkot", "Jacobabad", "Jamshoro", "Jungshahi", "Kandhkot", "Kandiaro", "Karachi", "Kashmore", "Keti Bandar", "Khairpur", "Kotri", "Larkana", "Matiari", "Mehar", "Mirpur Khas", "Mithani", "Mithi", "Mehrabpur", "Moro", "Nagarparkar", "Naudero", "Naushahro Feroze", "Naushara", "Nawabshah", "Nazimabad", "Qambar", "Qasimabad", "Ranipur", "Ratodero", "Rohri", "Sakrand", "Sanghar", "Shahbandar", "Shahdadkot", "Shahdadpur", "Shahpur Chakar", "Shikarpaur", "Sukkur", "Tangwani", "Tando Adam", "Tando Allahyar", "Tando Muhammad", "Thatta", "Umerkot", "Warah", "Abbottabad", "Adezai", "Alpuri", "Akora Khattak", "Ayubia", "Banda Daud", "Bannu", "Batkhela", "Battagram", "Birote", "Chakdara", "Charsadda", "Chitral", "Daggar", "Dargai", "Darya Khan", "dera Ismail", "Doaba", "Dir", "Drosh", "Hangu", "Haripur", "Karak", "Kohat", "Kulachi", "Lakki Marwat", "Latamber", "Madyan", "Mansehra", "Mardan", "Mastuj", "Mingora", "Nowshera", "Paharpur", "Pabbi", "Peshawar", "Saidu Sharif", "Shorkot", "Shewa Adda", "Swabi", "Swat", "Tangi", "Tank", "Thall", "Timergara", "Tordher", "Awaran", "Barkhan", "Chagai", "Dera Bugti", "Gwadar", "Harnai", "Jafarabad", "Jhal Magsi", "Kacchi", "Kalat", "Kech", "Kharan", "Khuzdar", "Killa Abdullah", "Killa Saifullah", "Kohlu", "Lasbela", "Lehri", "Loralai", "Mastung", "Musakhel", "Nasirabad", "Nushki", "Panjgur", "Pishin valley", "Quetta", "Sherani", "Sibi", "Sohbatpur", "Washuk", "Zhob", "Ziarat"];
+
+   allCities.forEach((city) => {
+      $('#namaz_time_form select[name=city]').append(`<option>${city}</option>`);
+   })
+
+});
+
+
+
+function showNamazTime(city) {
+   let counter = 0;
+   let name = ['Fajr', 'Dhuhr', 'Asr', 'Maghrib', 'Isha'];
+   $('#prayerTimes').html('');
+
+   $.getJSON(`https://muslimsalat.com/${city}/daily.json/?key=92d2a5ee0edd04dff0958008adaebb1f&jsoncallback=?`,
+      function (times) {
+
+         for (let i = 0; i < name.length; i++) {
+            $('#prayerTimes').append(`
+            <tr>
+               <th>${counter + 1}</th>
+               <td>${name[counter]}</td>
+               <td>${getNamazTime(counter, times)}</td>
+            </tr>
+         `);
+            counter++;
+         }
+
+      });
+
+   const getNamazTime = (namaz, times) => {
+      switch (namaz) {
+         case 0: return times.items[0].fajr;
+         case 1: return times.items[0].dhuhr;
+         case 2: return times.items[0].asr;
+         case 3: return times.items[0].maghrib;
+         case 4: return times.items[0].isha;
+      }
+
+   }
+}
+
